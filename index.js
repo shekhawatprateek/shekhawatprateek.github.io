0e231d442743f30dcdbc5729a07b7d9c77d3ce4e
Prateek Shekhawat <42514381+shekhawatprateek@users.noreply.github.com>
2021-03-11 13:19:30 +0530

    captcha authentication

--- index.js
+++ index.js
@@ -32,3 +32,12 @@ function setTheme(mode) {
 
     localStorage.setItem('theme', mode)
 }
+
+$("form").submit(function(event) {
+
+    var recaptcha = $("#g-recaptcha-response").val();
+    if (recaptcha === "") {
+       event.preventDefault();
+       alert("Please check the recaptcha");
+    }
+ });
